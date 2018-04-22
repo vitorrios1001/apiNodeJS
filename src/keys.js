868914ede4ffec1b5be4314e3b5689273ca27f5f
Vitor Rios <vr.vitorrios@gmail.com>
2018-04-22 17:09:02 -0300

    adding keys

---
+++ src/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    connectionString: 'mongodb://admin:admin@ds012678.mlab.com:12678/testemongodb',
+    sendgridKey: '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',
+    keysFirebase: {
+        apiKey: "AIzaSyC-4iKy1EGZKXlfnWh2-OaTheSP1S0x_rI",
+        authDomain: "apinodejs.firebaseapp.com",
+        databaseURL: "https://apinodejs.firebaseio.com",
+        projectId: "apinodejs",
+        storageBucket: "apinodejs.appspot.com",
+        messagingSenderId: "668855062239"
+    }
+}
