bb3f6e041e1cc3000fe5072a2159fc57816e80eb
Vitor Rios <vitorluizrios@hotmail.com>
2018-04-23 20:34:53 -0300

    Delete keys.js

--- src/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    connectionString: 'mongodb://admin:admin@ds012678.mlab.com:12678/testemongodb',
-    sendgridKey: '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',
-}
