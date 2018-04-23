beda1842233d12eb2fbb1c99838eab5d7fa0ad14
Vitor Rios <vr.vitorrios@gmail.com>
2018-04-23 20:32:08 -0300

    change secretKey

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-global.SALT_KEY = '5aa93bc125867b2329131fa4';
+global.SALT_KEY = process.env.SECRETKEY || keys.secretKey;
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong> seja bem vindo ao Node Storage';
 const keys = require('./keys');
 
